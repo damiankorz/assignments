4658d7a5810b1865675fc418542997209a4c649b
Damian Korzeniewski <damian.korz@gmail.com>
2018-03-04 22:59:42 -0600

    added filter by type

---
+++ python/fundamentals/filter-by-type.py
@@ -0,0 +1,46 @@
+# Filter by Type
+# Write a program that, given some value, tests that value for its type. Here's what you should do for each type:
+
+# Integer
+# If the integer is greater than or equal to 100, print "That's a big number!" If the integer is less than 100, print "That's a small number"
+
+# String
+# If the string is greater than or equal to 50 characters print "Long sentence." If the string is shorter than 50 characters print "Short sentence."
+
+# List
+# If the length of the list is greater than or equal to 10 print "Big list!" If the list has fewer than 10 values print "Short list."
+
+# Test your program using these examples:
+sI = 45
+mI = 100
+bI = 455
+eI = 0
+spI = -23
+sS = "Rubber baby buggy bumpers"
+mS = "Experience is simply the name we give our mistakes"
+bS = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+eS = ""
+aL = [1,7,4,21]
+mL = [3,5,7,34,3,2,113,65,8,89]
+lL = [4,34,22,68,9,13,3,5,7,9,2,12,45,923]
+eL = []
+spL = ['name','address','phone number','social security number']
+
+
+input = bI                                    #input variable
+if isinstance(input, int):                    #check if input is an interger
+    if input >= 100:                          #if yes, check if integer greater than or equal to 100
+        print "That's a big number!"          
+    else:                                     #if yes but interger is less than 100
+        print "That's a small number."
+if isinstance(input, str):                    #check if input is a string  
+    if len(input) >= 50:                      #if yes, check if charaters are greater than or equal to 50 
+        print "Long sentence."
+    else:                                     #if yes but characters are less than 50 
+        print "Short sentence."
+if isinstance(input, list):                   #check if input is a list
+    if len(input) >= 10:                      #if yes, check if the length is greater than or equal to 10
+        print "Big list!"
+    else:                                     #if yes but length is less than 10
+        print "Short list."
+       
