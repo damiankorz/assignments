27f85c02c3b3c38004986bad4ebb926b31a0293c
Damian Korzeniewski <damian.korz@gmail.com>
2018-03-14 20:54:08 -0500

    reworked code to make number game functional

--- python/flask/number-game/server.py
+++ python/flask/number-game/server.py
@@ -4,27 +4,29 @@ import random
 app = Flask(__name__)
 app.secret_key = "numb3rz"   
 
-# def genNum():
-#     session['num'] = random.randrange(1,101)
+def genNum():
+    session['num'] = random.randrange(1,101)
     
 @app.route('/')
 def index():
-    # if 'num' in session:
-    #     session['num'] = random.randrange(1,101)
-    #     genNum() 
-    # else: 
-    #     pass 
-    session['num'] = random.randrange(1,101)
+    if 'num' not in session:
+        genNum()
     return render_template('index.html')
 
 @app.route('/guess', methods=['POST'])
 def guess():
-    session['guess'] = int(request.form['guess'])
+    guess = int(request.form['guess'])
+    if guess < session['num']:
+        session['guess'] = "low"
+    elif guess > session['num']:
+        session['guess'] = "high"
+    else: 
+        session['guess'] = "correct"
     return redirect('/')
 
 @app.route('/reset', methods=['POST'])
 def reset():
-    genNum()
+    session.clear()
     return redirect('/')
 
 app.run(debug=True)
